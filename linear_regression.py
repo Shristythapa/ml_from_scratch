5a3d8f547c6a446db59666b6002c567602594a4f
Shristythapa <thapashristy110@gmail.com>
2025-04-27 08:40:29 +0545

    implemented linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class LinerRegression:
-    def __init__(self, learning_rate = 0.001, n_iters = 1000):
+    def __init__(self, learning_rate = 0.01, n_iters = 1000):
         self.learning_rate = learning_rate
         self.n_iters = n_iters
         self.weights = None
@@ -13,14 +13,20 @@ class LinerRegression:
         self.bias = 0
         
         for _ in range(self.n_iters):
+            
+            #caclulating y predict
             y_pred = np.dot(x, self.weights) +self.bias
             
+            #caclulating the derivative of weight
             dw = (1/n_samples) * np.dot(x.T,(y_pred-y))
+            #calculating the derivative of bias
             db = (1/n_samples) * np.sum(y_pred-y)
             
+            #updating the weight and bias i.e. implementing gradient descent
             self.weights = self.weights - self.learning_rate *dw
-            self.bias = self.bias - self.learning_rate * dw
+            self.bias = self.bias - self.learning_rate * db
             
     def predict (self,x):
+        #calculate prediction of y with final updated weights and biases
         y_pred = np.dot(x,self.weights)+self.bias
         return y_pred
